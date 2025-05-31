1463b02cdbdf3a3656f981f1d83af8d219fe9865
Yan Naung Oak <yannaungoak@gmail.com>
2025-05-31 11:46:50 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,102 @@
+data = {
+    "protests": [
+        {
+            "location": "Democracy Monument, Bangkok",
+            "date": "18 July 2020",
+            "source_text": "On 18 July 2020, Thailand saw the largest street demonstration since the 2014 Thai coup d'\u00e9tat at the Democracy Monument in Bangkok with around 2,500 protesters.",
+            "latitude": 13.7566891,
+            "longitude": 100.5019302
+        },
+        {
+            "location": "Democracy Monument, Bangkok",
+            "date": "16 August 2020",
+            "source_text": "On 16 August, a large gathering which around 20,000\u201325,000 people joined was held at the Democracy Monument and reiterated calls for a revised constitution and reforms to the monarchy.",
+            "latitude": 13.7566891,
+            "longitude": 100.5019302
+        },
+        {
+            "location": "Thammasat University, Bangkok",
+            "date": "19 September 2020",
+            "source_text": "In a rally described as one of the largest protests in years, on 19 September, protesters gathered at Thammasat University, then moved to Sanam Luang in the afternoon and stayed overnight there, with attendance estimated at anywhere between 20,000 and 100,000.",
+            "latitude": 13.7563309,
+            "longitude": 100.5017651
+        },
+        {
+            "location": "Pathum Wan Intersection, Bangkok",
+            "date": "16 October 2020",
+            "source_text": "On 16 October, around 2,000 unarmed protesters, mostly teenagers, gathered at Pathum Wan Intersection, and two hours later were dispersed by the police.",
+            "latitude": 13.7462321,
+            "longitude": 100.5307297
+        },
+        {
+            "location": "Ratchadamnoen Avenue, Bangkok",
+            "date": "14 October 2020",
+            "source_text": "The planned protest rally on 14 October began at Democracy Monument, with the objective of moving to Government House to demand the resignation of Prayut.",
+            "latitude": 13.8553428,
+            "longitude": 100.5454941
+        },
+        {
+            "location": "Srinakharinwirot University Ongkharak Campus, Nakhon Nayok",
+            "date": "February 2020",
+            "source_text": "Protests at Srinakharinwirot University Ongkharak Campus on 25 February.",
+            "latitude": 14.1074545,
+            "longitude": 100.9820801
+        },
+        {
+            "location": "Chiang Mai Province",
+            "date": "19 July 2020",
+            "source_text": "After 18 July, the protests began to spread across the country. The first were in Chiang Mai Province and Ubon Ratchathani Province on 19 July.",
+            "latitude": 18.7964642,
+            "longitude": 98.6600586
+        },
+        {
+            "location": "Ubon Ratchathani Province",
+            "date": "19 July 2020",
+            "source_text": "After 18 July, the protests began to spread across the country. The first were in Chiang Mai Province and Ubon Ratchathani Province on 19 July.",
+            "latitude": 15.0727086,
+            "longitude": 105.2194808
+        },
+        {
+            "location": "Maha Sarakham Province",
+            "date": "23 July 2020",
+            "source_text": "Some major demonstrations include one in Maha Sarakham Province on 23 July.",
+            "latitude": 16.1872403,
+            "longitude": 103.3044979
+        },
+        {
+            "location": "Nakhon Ratchasima Province",
+            "date": "24 July 2020",
+            "source_text": "One in Nakhon Ratchasima Province on 24 July saw one of the largest crowds amongst them.",
+            "latitude": 14.8882481,
+            "longitude": 102.2547919
+        },
+        {
+            "location": "Democracy Monument, Bangkok",
+            "date": "3 August 2020",
+            "source_text": "On 3 August, a Harry Potter-themed demonstration was held, which 200 people joined, featured a public speech by Arnon Nampa which openly criticised the monarchy.",
+            "latitude": 13.7566891,
+            "longitude": 100.5019302
+        },
+        {
+            "location": "Thammasat University, Rangsit campus, Pathum Thani Province",
+            "date": "10 August 2020",
+            "source_text": "On 10 August, there was a rally at Thammasat University, Rangsit campus in Pathum Thani Province named '\u0e18\u0e23\u0e23\u0e21\u0e28\u0e32\u0e2a\u0e15\u0e23\u0e4c\u0e08\u0e30\u0e44\u0e21\u0e48\u0e17\u0e19.' (lit. Thammasat will not tolerate.)",
+            "latitude": 14.0721931,
+            "longitude": 100.6056302
+        },
+        {
+            "location": "Democracy Monument, Bangkok",
+            "date": "26 July 2020",
+            "source_text": "On 26 July, an event called 'Let's run, Hamtaro' was organised at the Democracy Monument.",
+            "latitude": 13.7566891,
+            "longitude": 100.5019302
+        },
+        {
+            "location": "Sanam Luang, Bangkok",
+            "date": "19 September 2020",
+            "source_text": "On 19 September, protesters gathered at Thammasat University, then moved to Sanam Luang in the afternoon and stayed overnight there.",
+            "latitude": 13.7558693,
+            "longitude": 100.4932804
+        }
+    ]
+}
